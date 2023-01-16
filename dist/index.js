1e58e9faaafbd29d4d7a09fdfba193f961b6f0b3
Diego Breskovit <81165451+dbreskovit@users.noreply.github.com>
2023-01-16 10:09:48 -0300

    authentication@1.0.0 build

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const JWT_authentication_middleware_1 = __importDefault(require("./middleware/JWT-authentication.middleware"));
+const error_handler_middleware_1 = __importDefault(require("./middleware/error-handler.middleware"));
+const authorization_route_1 = __importDefault(require("./routes/authorization.route"));
+const status_route_1 = __importDefault(require("./routes/status.route"));
+const users_routes_1 = __importDefault(require("./routes/users.routes"));
 const app = (0, express_1.default)();
-app.get("/status", (req, res, next) => {
-    res.status(200).send("OK");
-});
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(status_route_1.default);
+app.use(authorization_route_1.default);
+app.use(JWT_authentication_middleware_1.default);
+app.use(users_routes_1.default);
+app.use(error_handler_middleware_1.default);
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
 });
